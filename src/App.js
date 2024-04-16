38ec49571ba93be77dad515f809af1226d291a87
mamaklangto <94784271+mamaklangto@users.noreply.github.com>
2024-04-17 02:25:49 +0800

    Added new features

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import  {BrowserRouter, Routes, Route}  from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
-import men_banner from './Components/Assets/banner_mens.png'
-import women_banner from './Components/Assets/banner_women.png'
-import kid_banner from './Components/Assets/banner_kids.png'
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
 
 function App() {
+  const [initialLoad, setInitialLoad] = useState(true);
+
+  useEffect(() => {
+    setInitialLoad(false);
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Shop/>} />
-          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
-          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
-          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
-          <Route path='/product' element={<Product/>}/>
-          <Route path='/product/:productId' element={<Product/>}/>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/login' element={<LoginSignup/>}/>
+          <Route
+            path="/"
+            element={
+              initialLoad ? (
+                <Navigate to="/shop" replace />
+              ) : (
+                <Shop />
+              )
+            }
+          />
+          <Route path="/shop" element={<Shop />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} category="kid" />}
+          />
+          <Route path="/product" element={<Product />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
